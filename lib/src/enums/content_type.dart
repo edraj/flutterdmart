e980a0940cbce08ecd18e2637cc10534ce4b1a61
Fatih Hamdani <fatih.hamdani@iq.zain.com>
2026-02-18 15:36:22 +0100

    content type update for image

--- lib/src/enums/content_type.dart
+++ lib/src/enums/content_type.dart
@@ -4,6 +4,11 @@ enum ContentType {
   markdown,
   json,
   image,
+  imageJpeg,
+  imagePng,
+  imageWebp,
+  imageGif,
+  imageSVG,
   python,
   pdf,
   audio,
@@ -12,5 +17,17 @@ enum ContentType {
   csv,
   sqlite,
   parquet,
-  apk,
+  apk;
+
+  static ContentType get(String value) {
+    return switch (value) {
+      "image_jpeg" || "image_jpg" => ContentType.imageJpeg,
+      "image_png" => ContentType.imagePng,
+      "image_svg" => ContentType.imageSVG,
+      "image_webp" => ContentType.imageWebp,
+      "image_gif" => ContentType.imageGif,
+      "image" => ContentType.image,
+      _ => ContentType.values.byName(value),
+    };
+  }
 }
